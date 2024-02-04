74f72e81835d8db445cedf55bc823d1f289db1bb
alieldeba <alieldeba375@gmail.com>
2024-02-05 00:35:46 +0200

    solving 1-5 assignments

---
+++ 1-5/1.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    This Is Multiple
+    Line Comment
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="<?php echo "UTF-8" ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?="My First PHP Page" ?></title>
+  </head>
+  <body>
+    <div><?php echo "We Love" ?></div>
+    <div><?php echo "Elzero Channe" ?></div>
+  </body>
+</html>
---
+++ 1-5/3.php
@@ -0,0 +1,4 @@
+<?php
+    echo "My Application";
+    echo "Version 1.0";
+    echo "Created By Elzero";
